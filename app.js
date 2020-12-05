6fb08fbc293d2be5164ca6f3a62ab5a6ece0cc88
michael tereschenko <michi.tereschenko@gmail.com>
2020-12-05 13:42:27 +0100

    time for simple commands

--- app.js
+++ app.js
@@ -5,21 +5,21 @@ const fs = require("fs");
 const app = express();
 port = process.env.PORT || 3000;
 app.listen(port, () => {
-	console.log(`listening at ${port}`);
+    console.log(`listening at ${port}`);
 });
 app.use(express.static("public"));
 app.use(express.json({limit:"1mb"}));
 
 // Define configuration options
 const opts = {
-  identity: {
-    username: "der_waschbaerbot",
-    password: process.env.password
-  },
-  channels: [
-    "datmatheeinhorn",
-    "c183649"
-  ]
+    identity: {
+        username: "der_waschbaerbot",
+        password: process.env.password || "oauth:6q5g0j0cyqzl04f77okfrt1tp1ifoh"
+    },
+    channels: [
+        "datmatheeinhorn",
+        "c183649"
+    ]
 };
 
 // Create a client with our options
@@ -32,38 +32,63 @@ client.on('connected', onConnectedHandler);
 // Connect to Twitch:
 client.connect();
 
-function timedMessage() {
-  client.say("Ich sage das alle 5 Minuten");
+function timedMessage(streamer) {
+    client.say(streamer, "Ich sage das alle 5 Minuten");
 }
 
-let passedMessages = {};
+let oldMessages = {};
 
-function strike(target) {
-  // STRIKE SYSTEM
+function strike(streamer, user, reason) {
+    client.timeout(streamer,user, 5, reason);
+}
+
+function count(item, list) {
+    let c = 0;
+    for (let i of list) {
+        if (i == item) {
+            c++;
+        }
+    }
+    return c;
+}
+
+function find(substr, longstr) {
+    for (let i = 0; i < longstr.length-substr.length+1; i++) {
+        if (longstr.substr(i, substr.length) == substr) {
+            return true;
+        }
+    }
+    return false;
 }
 
 // Called every time a message comes in
-function onMessageHandler (target, context, msg, self) {
-  if (self) { return; } // Ignore messages from the bot
-  console.log(context);
-  // Remove whitespace from chat message
-  const commandName = msg.trim();
-  if (!passedMessages[target]) {
-    passedMessages[target] = [msg];
-  } else {
-    passedMessages[target].push(msg);
-  }
-  if (count(msg, passedMessages[target]) > 3) {
-    strike(target);
-  }
+function onMessageHandler (streamer, context, msg, self) {
+    if (self) { return; } // Ignore messages from the bot
+    let user = context.username;
+    console.log(streamer, user, context, msg);
+    if (find("nightbot", msg)) {
+        client.say(streamer, "Nightbot ist tot! Ich hab ihn umgebracht! >:)");
+    }
+    // Remove whitespace from chat message
+    const commandName = msg.trim();
+    if (!oldMessages[user]) {
+        oldMessages[user] = [msg];
+    } else {
+        oldMessages[user].push(msg);
+    }
+    if (count(msg, oldMessages[user]) > 3) {
+        strike(streamer, name, "bitte hoer auf diese nachricht zu senden");
+    }
 
-  // If the command is known, let's execute it
-  // client.say(target, answer);
+  // simple commands (no arguments)
+  // client.say(streamer, answer);
   switch(commandName) {
-    case "":
-    break
-    // case "":
-    // break
+    case "!hallo":
+        client.say(streamer, user+", hallo!");
+        break
+    case "!timemeout":
+        strike(streamer, user, "du wolltest es so");
+        break
     // case "":
     // break
     // case "":
@@ -81,6 +106,6 @@ function onMessageHandler (target, context, msg, self) {
 
 // Called every time the bot connects to Twitch chat
 function onConnectedHandler (addr, port) {
-  console.log(`* Connected to ${addr}:${port}`);
-  setInterval(timedMessage, 5*60*1000);
+    console.log(`* Connected to ${addr}:${port}`);
+    setInterval(timedMessage, 5*60*1000);
 }
